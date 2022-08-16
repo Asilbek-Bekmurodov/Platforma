56e274cc5ccac2b457639088214eece0d1bd8bd5
Asilbek2002 <school2924@gmail.com>
2022-08-16 20:26:37 +0500

    4.3 vazifa

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./app";
 import "./index.css";
-import App from "./1-vazifa/App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
