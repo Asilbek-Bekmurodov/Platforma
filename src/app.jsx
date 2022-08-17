41b6a371d97297ec21fdce36d0b09abfe5d14346
Asilbek2002 <school2924@gmail.com>
2022-08-17 20:31:46 +0500

    new project

--- src/app.jsx
+++ src/app.jsx
@@ -1,48 +1,9 @@
-import React, { Component } from "react";
-import Cards from "./components/cards/cards";
-import Footer from "./components/footer";
-import Main from "./components/main";
-import Navbar from "./components/navbar";
+import React, { Component } from 'react'
 
 export default class App extends Component {
-  state = {
-    cardsData: [
-      {
-        title: "Free",
-        price: 0,
-        usersCount: 10,
-        gb: 2,
-        support: "Email ",
-        btnName: "Sign up for free"
-      },
-      {
-        title: "Pro",
-        price: 15,
-        usersCount: 20,
-        gb: 10,
-        support: "Priority email ",
-        btnName: "Get started",
-      },
-      {
-        title: "Enterprise",
-        price: 29,
-        usersCount: 30,
-        gb: 15,
-        support: "Phone and email ",
-        btnName: "Contact us",
-      },
-    ],
-  };
   render() {
-    const { cardsData } = this.state;
     return (
-      <div className="container">
-        <Navbar />
-        <Main />
-        <Cards setData={cardsData} />
-        <Footer/>
-      </div>
-    );
+      <div>App</div>
+    )
   }
 }
-
